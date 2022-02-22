d33deec6f9e5c6dc523f726b98ad424257e18715
Caio Lucas M. de Moraes <caio_mesquita@virgo.atlantico.net.br>
2022-02-22 16:38:47 -0300

    Adding random class

---
+++ feigndemo/src/main/java/com/atlantico/feigndemo/afterclassdeleted/GitTraining.java
@@ -0,0 +1,8 @@
+package com.atlantico.feigndemo.afterclassdeleted;
+
+public class GitTraining {
+
+    public void anyMethod {
+        System.out.println("Essa classe foi adicionada após outra ter sido deletada para testar git");
+    }
+}
